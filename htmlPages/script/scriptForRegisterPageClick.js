c2a54675a87dfe5f04576f6da5b9eb6c6c9a3d7b
IgorTysiachuk <igortysiachuk05@gmail.com>
2023-11-24 21:21:24 +0200

    0.11

--- htmlPages/script/scriptForRegisterPageClick.js
+++ htmlPages/script/scriptForRegisterPageClick.js
@@ -1,7 +1,28 @@
 const buttonLogo = document.querySelector('#educationLogoClick')
 const startPage = './index.html'
 
+const blockForRole = document.querySelectorAll('#blockForRole')
+const pointForRole = document.querySelectorAll('#pointForRole')
 buttonLogo.addEventListener('click', () => {
-    console.log(123)
     window.location.href = startPage;
+})
+
+let chosenRole = 0 // 1 - student  2 - teacher  3 - owner  0 - nobody in this life(
+
+const renderingRole = () => {
+    pointForRole.forEach((item) => {
+        item.innerHTML = ``
+    })
+    blockForRole.forEach((item) => {
+        item.classList.remove('choosen')
+    })
+    pointForRole[chosenRole-1].innerHTML = `<div class="register_block_second_category_block1_top_point_into"></div>`
+    blockForRole[chosenRole-1].classList.add('choosen')
+}
+
+blockForRole.forEach((item, index) => {
+    item.addEventListener('click', () => {
+        chosenRole = index+1
+        renderingRole()
+    })
 })
